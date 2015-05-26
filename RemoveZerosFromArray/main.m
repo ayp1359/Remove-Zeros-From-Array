019d7b7fffdf1dd0bac747e490a4ade489927d93
Ali Panah <ayp@fb.com>
2015-05-25 20:23:23 -0700

    first commit

--- RemoveZerosFromArray/main.m
+++ RemoveZerosFromArray/main.m
@@ -1,17 +1,39 @@
-//
-//  main.m
-//  RemoveZerosFromArray
-//
-//  Created by Ali Panah on 5/25/15.
-//  Copyright (c) 2015 Ali Panah. All rights reserved.
-//
+//Program that brings all nonzero elements to the left of the array & returns the number of nonzero elements.
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-  @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+NSUInteger removeZeros(NSMutableArray *numberArray){
+  
+  NSUInteger leftIndex = 0;
+  NSUInteger rightIndex = numberArray.count-1;
+  
+  while (leftIndex <= rightIndex) {
+    
+    if([numberArray[leftIndex] integerValue] != 0){
+      leftIndex++;
+    }
+    else if ([numberArray[rightIndex] integerValue] == 0){
+      rightIndex--;
+    }
+    else {
+      [numberArray replaceObjectAtIndex:leftIndex withObject:numberArray[rightIndex]];
+      leftIndex++;
+      rightIndex--;
+    }
+    
   }
-    return 0;
+  
+  return leftIndex;
+}
+
+int main(int argc, const char *argv[]){
+  @autoreleasepool {
+    
+    NSMutableArray *numberArray = [[NSMutableArray alloc] initWithArray:@[@1,@0,@0,@4,@0,@5,@9,@2,@0,@6,@0,@0]];
+    NSUInteger numNonZeroElements = removeZeros(numberArray);
+    NSLog(@"Array: %@, number of non-zero elements: %lu",numberArray, (unsigned long)numNonZeroElements);
+    
+  }
+  
+  return 0;
 }
