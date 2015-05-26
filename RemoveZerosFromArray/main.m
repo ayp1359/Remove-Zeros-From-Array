f9205e157a086d880901a6fb0a5af49bd8ab2e04
Ali Panah <ayp@fb.com>
2015-05-25 20:03:33 -0700

    Initial Commit

---
+++ RemoveZerosFromArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RemoveZerosFromArray
+//
+//  Created by Ali Panah on 5/25/15.
+//  Copyright (c) 2015 Ali Panah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
